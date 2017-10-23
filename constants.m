204b1b107ebdb69390e3b87f51213769181e5028
Albin Ohlsson <albin_ohlsson@hotmail.com>
2017-10-23 06:11:38 +0200

    apparently it can't be triggered

---
+++ constants.m
@@ -0,0 +1,25 @@
+downDCDelta = -15;
+
+downDCMax = 255;
+
+downDCThreshold = -100;
+
+floor1dist = 10;
+
+floor2dist = 20;
+
+floor3dist = 30;
+
+floor4dist = 40;
+
+maxDistThreshold = 50;
+
+minDistThreshold = 5;
+
+upDCDelta = 15;
+
+upDCMax = 255;
+
+upDCThreshold = 100;
+
+margin = 1;
--- setDirection.m
+++ setDirection.m
@@ -1,21 +1,25 @@
 function dir = setDirection(currentFloor, pressed, direction)
-   if direction = 0
+   if direction == 0
        if currentFloor == 1 && (pressed(2) || pressed(3) || pressed(4))
-           dir = 1
+           dir = 1;
        elseif currentFloor == 2 && (pressed(3) || pressed(4))
-           dir = 1
+           dir = 1;
        elseif currentFloor == 3 && pressed(4)
-           dir = 1
+           dir = 1;
        elseif currentFloor == 4 && (pressed(1) || pressed(2) || pressed(3))
-           dir = -1
+           dir = -1;
        elseif currentFloor == 3 && (pressed(1) || pressed(2))
-           dir = -1
+           dir = -1;
        elseif currentFloor == 2 && pressed(1)
-           dir = -1
+           dir = -1;
        end
+   else
+       dir = direction;
    end
    if ~pressed(1) && ~pressed(2) && ~pressed(3) && ~pressed(4)
-           dir = 0
+           dir = 0;
+   else
+       dir = 0;
    end
 end
     
